49af57a03c99bd3ded34ab26ff479920fe35ac6c
Mogakamo <mogaka.amo254@gmail.com>
2021-09-18 21:08:30 +0000

    setting up bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,13 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import BootstrapVue from "bootstrap-vue"; // Import Bootstrap here
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+Vue.use(BootstrapVue);
+
 Vue.config.productionTip = false;
 
 new Vue({
