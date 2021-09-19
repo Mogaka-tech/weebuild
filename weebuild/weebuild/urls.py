c5830b42e5da8665806526ac1ac7b5ec14e4525f
Mogakamo <mogaka.amo254@gmail.com>
2021-09-19 10:58:06 +0000

    setting up django

--- weebuild/weebuild/urls.py
+++ weebuild/weebuild/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from vue_app import views as vue_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('test', vue_views.test_vue),
 ]
