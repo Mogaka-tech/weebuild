1bffb42e1cc03431e16c7ce988807c7f7a836964
Mogakamo <mogaka.amo254@gmail.com>
2021-10-03 12:43:44 +0000

    commit

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,13 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import * as Sentry from "@sentry/vue";
-import { Intergrations } from "@sentry/tracing";
+import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
   App,
   dsn: "https://6ff98cb448c64b0eb12df5e592dc8ba0@o573566.ingest.sentry.io/5990169",
-  Intergrations: [
-    new Intergrations.BrowserTracing({
+  Integrations: [
+    new Integrations.BrowserTracing({
       routingInstrumentation: Sentry.vueRouterInstrumentation(router),
       tracingOrigins: ["localhost", "my-site-url.com", /^\//],
     }),
