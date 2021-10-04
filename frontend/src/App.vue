cf6071d210c81492b8bc029860d66e95dfd68be4
Mogakamo <mogaka.amo254@gmail.com>
2021-10-04 01:43:38 +0000

    commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,9 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,5 +22,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
